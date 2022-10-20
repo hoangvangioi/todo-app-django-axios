d8b3652ee1a6bdf12909b6280c21c939339efcf1
Hoang Van Gioi <gioitube2k2@gmail.com>
2022-10-20 08:30:56 +0700

    Deploy heroku

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 ## To do app
 
 # API django rest framework
-
+Deploy heroku
 
 
 # Call API with axios
-
+Deploy nettify
--- todoapp/settings.py
+++ todoapp/settings.py
@@ -138,6 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5500',
+    'https://todo.hoangvangioi.xyz'
 ]
 
 # Activate Django-Heroku.
