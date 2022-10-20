c535b94841582a3736ecd4f354528e0eb5dc9a36
Hoang Van Gioi <gioitube2k2@gmail.com>
2022-10-20 23:39:15 +0700

    update delete task

--- api/views.py
+++ api/views.py
@@ -71,7 +71,7 @@ class TaskDestroyAPIView(RetrieveDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    # def destroy(self, request, *args, **kwargs):
+    #     instance = self.get_object()
+    #     self.perform_destroy(instance)
+    #     return Response(status=status.HTTP_204_NO_CONTENT)
